61d6c6f1d20c1d03c1ad29cde0659b08909d73dd
Magomed Alimkhanov <scrythepvp@gmail.com>
2025-12-12 23:28:44 +0100

    feat: add bit code for dynamically add/set array
    incomplete code to dynamically add or change item of array, but will
    maybe remove to keep more simple

--- src/main/java/dev/scrythe/customlag/commands/ConfigCommand.java
+++ src/main/java/dev/scrythe/customlag/commands/ConfigCommand.java
@@ -1,21 +1,27 @@
 package dev.scrythe.customlag.commands;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.*;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import dev.scrythe.customlag.config.ConfigOption;
 import dev.scrythe.customlag.config.CustomLagConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Map;
 
 public class ConfigCommand {
-    private static final Map<Class<?>, ArgumentType<?>> argumentTypeMap = Map.of(int.class, IntegerArgumentType.integer(), Double.class, DoubleArgumentType.doubleArg(), Float.class, FloatArgumentType.floatArg(), String.class, StringArgumentType.string(), boolean.class, BoolArgumentType.bool(), long.class, LongArgumentType.longArg());
+    private static final Map<Class<?>, ArgumentType<?>> argumentTypeMap = Map.of(Integer.class, IntegerArgumentType.integer(), boolean.class, BoolArgumentType.bool(), long.class, LongArgumentType.longArg(), ServerPlayer.class, EntityArgument.player());
 
     public static LiteralArgumentBuilder<CommandSourceStack> register() {
         LiteralArgumentBuilder<CommandSourceStack> customlagCommand = Commands.literal("customlag");
@@ -35,7 +41,7 @@ public class ConfigCommand {
             }
             fieldCommand = fieldCommand.then(resetCommand(field));
             if (annotation.add()) {
-                fieldCommand = fieldCommand.then(addCommand(field));
+                fieldCommand = fieldCommand.then(setArrayCommand(field));
             }
             if (annotation.remove()) {
                 fieldCommand = fieldCommand.then(removeCommand());
@@ -48,33 +54,39 @@ public class ConfigCommand {
     }
 
     private static LiteralArgumentBuilder<CommandSourceStack> getCommand(Field field) {
-        return Commands.literal("get").executes(context -> {
-            try {
-                Object fieldValue = field.get(CustomLagConfig.class);
-                context.getSource()
-                        .sendSuccess(() -> Component.literal("%s is set to %s".formatted(field.getName(), fieldValue)), false);
-            } catch (IllegalAccessException e) {
-                context.getSource().sendFailure(Component.literal(e.toString()));
-            }
-            return 1;
-        });
+        return Commands.literal("get").executes(context -> executeGetCommand(context, field));
+    }
+
+    private static int executeGetCommand(CommandContext<CommandSourceStack> context, Field field) {
+        try {
+            Object fieldValue = field.get(CustomLagConfig.class);
+            context.getSource()
+                    .sendSuccess(() -> Component.literal("%s is set to %s".formatted(field.getName(), fieldValue)), false);
+            return Command.SINGLE_SUCCESS;
+        } catch (IllegalAccessException e) {
+            context.getSource().sendFailure(Component.literal(e.toString()));
+            return 0;
+        }
     }
 
     private static LiteralArgumentBuilder<CommandSourceStack> setCommand(Field field) {
         ArgumentType<?> argumentType = argumentTypeMap.getOrDefault(field.getType(), StringArgumentType.string());
         RequiredArgumentBuilder<CommandSourceStack, ?> argumentCommand = Commands.argument(field.getName(), argumentType)
-                .executes(context -> {
-                    Object fieldValue = context.getArgument(field.getName(), field.getType());
-                    try {
-                        field.set(CustomLagConfig.class, fieldValue);
-                        context.getSource()
-                                .sendSuccess(() -> Component.literal("Set %s to %s".formatted(field.getName(), fieldValue)), false);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return 1;
-                });
+                .executes(context -> executeSetCommand(context, field));
         return Commands.literal("set").then(argumentCommand);
+    }
+
+    private static int executeSetCommand(CommandContext<CommandSourceStack> context, Field field) {
+        Object fieldValue = context.getArgument(field.getName(), field.getType());
+        try {
+            field.set(CustomLagConfig.class, fieldValue);
+            context.getSource()
+                    .sendSuccess(() -> Component.literal("Set %s to %s".formatted(field.getName(), fieldValue)), false);
+            return Command.SINGLE_SUCCESS;
+        } catch (IllegalAccessException e) {
+            context.getSource().sendFailure(Component.literal(e.toString()));
+            return 0;
+        }
     }
 
     private static LiteralArgumentBuilder<CommandSourceStack> resetCommand(Field field) {
@@ -84,34 +96,50 @@ public class ConfigCommand {
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-        return Commands.literal("reset").executes(context -> {
-            try {
-                field.set(CustomLagConfig.class, defaultValue);
-                context.getSource()
-                        .sendSuccess(() -> Component.literal("Reset %s to %s (default)".formatted(field.getName(), defaultValue)), false);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-            return 1;
-        });
+        return Commands.literal("reset").executes(context -> executeResetCommand(context, field, defaultValue));
     }
 
-    private static LiteralArgumentBuilder<CommandSourceStack> addCommand(Field field) {
-        Object fieldType = field.getType().getComponentType();
-        ArgumentType<?> argumentType = argumentTypeMap.getOrDefault(fieldType, StringArgumentType.string());
-        RequiredArgumentBuilder<CommandSourceStack, ?> argumentCommand = Commands.argument(field.getName(), argumentType)
-                .executes(context -> {
-                    Object fieldValue = context.getArgument(field.getName(), field.getType());
-                    try {
-                        field.set(CustomLagConfig.class, fieldValue);
-                        context.getSource()
-                                .sendSuccess(() -> Component.literal("Added %s to %s".formatted(fieldValue, field.getName())), false);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return 1;
-                });
-        return Commands.literal("add").then(argumentCommand);
+    private static int executeResetCommand(CommandContext<CommandSourceStack> context, Field field, Object defaultValue) {
+        try {
+            field.set(CustomLagConfig.class, defaultValue);
+            context.getSource()
+                    .sendSuccess(() -> Component.literal("Reset %s to %s (default)".formatted(field.getName(), defaultValue)), false);
+            return Command.SINGLE_SUCCESS;
+        } catch (IllegalAccessException e) {
+            context.getSource().sendFailure(Component.literal(e.toString()));
+            return 0;
+        }
+    }
+
+    private static LiteralArgumentBuilder<CommandSourceStack> setArrayCommand(Field field) {
+        ParameterizedType mapType = (ParameterizedType) field.getGenericType();
+        Type[] mapTypeArguments = mapType.getActualTypeArguments();
+
+        Class<?> keyType = (Class<?>) mapTypeArguments[0];
+        Class<?> valueType = (Class<?>) mapTypeArguments[1];
+
+        ArgumentType<?> keyArgumentType = argumentTypeMap.getOrDefault(keyType, StringArgumentType.string());
+        ArgumentType<?> valueArgumentType = argumentTypeMap.getOrDefault(valueType, StringArgumentType.string());
+
+        return Commands.literal("set")
+                .then(Commands.argument(field.getName(), keyArgumentType)
+                        .then(Commands.argument(field.getName(), valueArgumentType)
+                                .executes(context -> executeSetArrayCommand(context, field, keyType, valueType))));
+    }
+
+    private static int executeSetArrayCommand(CommandContext<CommandSourceStack> context, Field field, Class<?> keyType, Class<?> valueType) {
+        Object keyValue = context.getArgument(field.getName(), keyType);
+        Object valueValue = context.getArgument(field.getName(), valueType);
+        try {
+            Map<Object, Object> map = (Map<Object, Object>) field.get(CustomLagConfig.class);
+            map.put(keyValue, valueValue);
+            context.getSource()
+                    .sendSuccess(() -> Component.literal("Set %s with value %s to %s".formatted(keyValue, valueValue, field.getName())), false);
+            return Command.SINGLE_SUCCESS;
+        } catch (IllegalAccessException e) {
+            context.getSource().sendFailure(Component.literal(e.toString()));
+            return 0;
+        }
     }
 
     private static LiteralArgumentBuilder<CommandSourceStack> removeCommand() {
--- src/main/java/dev/scrythe/customlag/config/CustomLagConfig.java
+++ src/main/java/dev/scrythe/customlag/config/CustomLagConfig.java
@@ -1,21 +1,63 @@
 package dev.scrythe.customlag.config;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dev.scrythe.customlag.DelayHandler.DelayingChannelDuplexHandler;
+import dev.scrythe.customlag.mixin.ConnectionAccessor;
+import dev.scrythe.customlag.mixin.ServerCommonPacketListenerImplAccessor;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelPipeline;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class CustomLagConfig {
     @Comment("Whether to use only one Ping/Keepalive packet to determine the latency or the average of the latest 4.")
-    @ConfigOption()
+    @ConfigOption
     public static boolean useOnlyOnePingPacket = false;
 
 
     @Comment("Custom Ping/Keepalive Packet Interval. Sending more frequent Ping Packets (but also reducing the time when a player gets kicked).")
     @Comment("Set to -1 to use default.")
-    @ConfigOption()
+    @ConfigOption
     public static long pingSendInterval = -1;
 
 
     @Comment("Show a numeral ping instead of the ping bar (only for client).")
     @ConfigOption(client = true)
     public static boolean showNumeralPing = false;
+
+    @ConfigOption(add = true)
+    public static Map<ServerPlayer, Integer> playerLagMap = new HashMap<>();
+
+    public static void setPlayerMap(CommandContext<CommandSourceStack> context) {
+        ServerPlayer player;
+        try {
+            player = EntityArgument.getPlayer(context, "player");
+        } catch (CommandSyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        String playerName = player.getName().getString();
+        int latency = IntegerArgumentType.getInteger(context, "latency");
+
+        Connection connection = ((ServerCommonPacketListenerImplAccessor) player.connection).getConnection();
+        Channel channel = ((ConnectionAccessor) connection).getChannel();
+        ChannelPipeline channelPipeline = channel.pipeline();
+        ChannelHandler channelHandler = channelPipeline.get("delay_handler");
+
+        if (channelHandler instanceof DelayingChannelDuplexHandler delayingChannelDuplexHandler) {
+            delayingChannelDuplexHandler.changeLatency(latency);
+        } else {
+            DelayingChannelDuplexHandler delayingChannelDuplexHandler = new DelayingChannelDuplexHandler(latency);
+            channelPipeline.addBefore("packet_handler", "delay_handler", delayingChannelDuplexHandler);
+        }
+
+        playerLagMap.put(player, latency);
+    }
 }
